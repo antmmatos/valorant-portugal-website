09d190e531e8fd8fac5f1688357a852c874fe12c
António Matos <antonio.martim.matos@gmail.com>
2023-09-01 17:51:34 +0100

    Criação da navbar

---
+++ src/components/main_navbar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import styles from "./main_navbar.module.css";
+
+export default function MainNavbar() {
+    return (
+        <nav className={styles.nav}>
+            <ul className={styles.container}>
+                <li className={styles.element}>
+                    <a href="/">Home</a>
+                </li>
+                <li className={styles.element}>
+                    <a href="/about">About</a>
+                </li>
+                <li className={styles.element}>
+                    <a href="/staff">Staff</a>
+                </li>
+                <ul className={styles.center}>
+                    <Image src="/logo.png" alt="logo" width={150} height={150} />
+                </ul>
+                <ul className={styles.right}>
+                    <li className={styles.element}>
+                        <a href="/login">Login</a>
+                    </li>
+                    <li className={styles.element}>
+                        <a href="/register">Register</a>
+                    </li>
+                </ul>
+            </ul>
+        </nav>
+    );
+}
