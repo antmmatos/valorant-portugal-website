3ebe12a16e79a953ad55aa8cba88305f02a60beb
António Matos <antonio.martim.matos@gmail.com>
2023-09-01 21:21:39 +0100

    Alterações navbar

--- src/app/regulamento/page.tsx
+++ src/app/regulamento/page.tsx
@@ -1,157 +1,134 @@
 import styles from "./page.module.css";
 
 export default function Regulamento() {
-    return <main className={styles.main}>
-
-        <div className={styles.container}>
-        <h1 className={styles.regulamento}>Regulamento</h1>
-<p className={styles.textRegulamento}>
-1 - Lobby Personalizado
-
-1.1 - Para cada jogo um responsável irá convidar os respetivos líderes de equipas que irão jogar. Posteriormente os líderes deverão convidar os seus jogadores.
-
-Definições do Lobby Personalizado:
-
-Modo: Padrão
-Servidores: Madrid.
-Cheats: Off
-Modo Torneio: On
-Prorrogação - Venca por dois: On
-Servidor: Madrid
-
-1.2 - Todos os jogadores terão que estar presentes no lobby dentro do horário estipulado para o jogo (30 minutos de tolerância de atraso). Se os jogadores não estiverem presentes depois do limite, a equipa com todos os membros pode tirar uma captura de ecrã ao Lobby e reportar o resultado (Vitória por falta de comparência).
-
-1.3 - Jogos serão transmitido ou gravados por algum observador da equipa VLRPT
-2 - Equipas e Roster
-
-2.1 - Cada equipa deve conter no mínimo 5 jogadores.
-
-2.2 - Cada roster pode conter 5 jogadores e 2 substitutos e 1 treinador.
-
-2.3 - Um jogador substituto pode ser adicionado antes ou depois de um jogo/mapa completo.
-
-2.4 - O substituto não pode pertencer a mais nenhuma equipa que esteja a participar ou tenha sido eliminada no mesmo Torneio.
-
-2.5 - Qualquer jogador do roster que não esteja a participar no jogo, pode preencher a vaga do treinador da equipa no lobby.
-
-2.6 - Cada jogador só pode representar uma equipa no mesmo Torneio.
-3 - Exploits e Cheats
-
-3.1 - Qualquer modificação de terceiros que crie uma injusta vantagem vai resultar na remoção imediata da equipa. Isto não sendo permitido:
-
-Multihacks
-Wallhack
-Aimbot
-Modelos Coloridos
-Sem Recoil
-Sem Flash
-Mudanças de Sons
-
-3.2 - Todos os cheats estão estritamente proibidos em qualquer circunstancia do Torneio. Se um utilizador for apanhado a usar qualquer tipo de cheats irá resultar em Ban e desqualificação da equipa. Os seguintes cheats incluídos são:
-
-ESP
-Aim hacks
-Speed hacks
-Wallhacks
-Radar hacks
-Hitbox manipulação
-Teletransporte (qualquer tipo de transporte que não seja disponível no jogo)
-Uso de qualquer bug/bugs para ganhar vantagem contra os seus oponentes.
-
-3.3 - Caso seja constatado que alguém está abusando qualquer uma das regras, as seguintes penalidades podem ser aplicadas:
-
-Aviso
-Vitória para o adversário
-Desqualificação do torneio
-
-3.4 - Ferramentas de sobreposição de terceiros não são permitidas. Exemplos:
-
-Tracker
-Blitz
-Custom Crosshair
-4 - Pausas e Desconexões
-
-4.1 - As pausas técnicas são apenas para dificuldades técnicas ou desconexões durante o início da ronda (fase de compra).
-
-4.2 - Na eventualidade de um jogador se desconectar a meio de uma ronda a ronda deverá continuar como normal. Quando a ronda acabar e a nova ronda começar o host pausa o lobby na fase de compra permitindo ao jogador/es se reconectem, voltando ao lobby, a equipa com problemas de conexão devem informar à outra equipa que estão prontos para continuar.
-
-4.3 - As pausas técnicas tem uma duração de 10 minutos (máximos). Caso seja necessário um tempo extra cabe ao observador da partida decidir se é justificável ou não. Quando ambas as equipas estiverem prontas para continuar devem usar o chat in-game para informar o observador (nunca o party voice chat). Usar a pausa técnica como pausa tática é proíbido.
-
-4.4 - As equipas podem o Timeout in-game de 2x um minuto de pausa. Só podem usar uma por cada lado. Isto é permitido para todas as fases do Torneio. (Não devem usar a pausa do timer ingame para pausas táticas)
-
-4.5 - No caso de Prorrogação, cada equipa irá ter o direito de mais uma pausa tática para usar na prorrogação.
-5 - Streaming
-
-5.1 – Os jogadores podem fazer live stream dos seus POVs durante os qualificadores abertos, desde que cumprem os requisitos. Jogadores a fazer Streaming tem de ter a live stream muda durante conversas no Discord com a Staff, incluindo encontros com os líderes antes do torneio começar. Treinadores, equipa staff e criadores de conteúdo não contam como jogadores competidores, e por tal não podem fazer stream.
-
-5.2 - É permitido realizar Watch party’s do torneio através de uma stream de um observador.
-
-5.3 - Todos os jogos das finais ao vivo do Torneio serão transmitidos nos canais próprios do Torneio. Apenas parceiros de produção aprovados e jogadores concorrentes poderão transmitir o evento principal.
-
-5.4 - Os jogadores que transmitirem o evento serão obrigados a usar um atraso mínimo de 240 segundos para manter a integridade competitiva. Qualquer pessoa que transmita o torneio deve desativar a configuração "mostrar sangue" e "mostrar corpos", de acordo com as diretrizes da Riot.
-
-5.5 - Os jogadores que transmitirem o evento também devem incluir o logotipo do Torneio em sua sobreposição e utilizar o título do evento no título da transmissão. Infelizmente, as equipes incapazes de atender a essa solicitação não poderão transmitir.
-
-5.6 - Para todos os indivíduos que desejam transmitir, solicitamos educadamente o relatório de todas as estatísticas após o evento.
-6 - FORMATO
-
-6.1 - Divisão de equipas entre 2 ligas ( 「🏆」primeira-liga / 「🏆」segunda-liga ) de 8 equipas
-
-6.2 - Confrontos no formato Round Robin (todos jogam contra todos) com 1 volta
-
-6.3 - Dependendo da posição na tabela da equipa no final da volta serão atribuídos pontos do circuito VLRPT que, posteriormente, darão a possibilidade de qualificar para um evento futuro
-
-6.4 - A despromoção e promoção de equipas da 1° para a 2° liga, e vice-versa, será feita através de uma final four entre o 7° e 8° lugar da 1° liga e o 1° e 2° da 2° liga (double elimination bracket)
-
-6.5 - A qualificação para as ligas poderá ser feita através de um qualificador realizado ao mesmo tempo que as ligas (single elimination bracket) no qual apenas as equipas campeã e vice-campeã poderão aceder a uma final four contra o 7° e 8° da 2° liga
-
-6.6 - A falta de comparência de 2 ou mais equipas das ligas custará no uso dos resultados atuais ( caso não se tenha terminado a primeira volta) ou os resultados da primeira volta (caso já tenha sido concluída)
-7 - Map poll and veto
-
-7.1 - Map poll atual:
-
-Ascent
-Fracture
-Haven
-Icebox
-Lótus
-Pearl
-Split
-
-7.2 - Processo do Veto:
-(o processo de veto poderá alterar face a um update de mapas no Valorant ou restrição da Riot)
-
-Em jogos à melhor de um (BO1):
-
-Ban equipa A
-Ban equipa B
-Ban equipa A
-Ban equipa B
-Ban equipa A
-Ban equipa B
-Side pick equipa A
-
-Em jogos à melhor de três (Bo3):
-
-Ban equipa A
-Ban equipa B
-Map pick equipa A
-Side pick equipa B
-Map pick equipa B
-Side pick equipa A
-Ban equipa A
-Ban equipa B
-Side pick equipa A
-
-7.3 - Novos agentes terão um período de experiência de quatro (4) semanas. Se o torneio começar durante este período de experiência, o agente será proibido pelo resto do torneio.
-8 - Conduta do Jogador
-
-8.1 - Espera-se que os jogadores tratem outros Competidores, Staff e Equipa de produção com respeito em qualquer altura.
-
-8.2 - Nomes de equipes, nomes de jogadores, crosshairs e logotipos não devem ser ofensivos, tóxicos, inapropriado ou prejudiciais.
-
-Para a aplicação de todas as regras ou raras circunstâncias que possam estar fora das regras, a decisão da Staff é final.
-</p>
-        </div>
-        </main>;
+    return (
+        <main className={styles.main}>
+            <div className={styles.container}>
+                <h1 className={styles.regulamento}>Regulamento</h1>
+                <p className={styles.textRegulamento}>
+                    1 - Lobby Personalizado 1.1 - Para cada jogo um responsável
+                    irá convidar os respetivos líderes de equipas que irão
+                    jogar. Posteriormente os líderes deverão convidar os seus
+                    jogadores. Definições do Lobby Personalizado: Modo: Padrão
+                    Servidores: Madrid. Cheats: Off Modo Torneio: On Prorrogação
+                    - Venca por dois: On Servidor: Madrid 1.2 - Todos os
+                    jogadores terão que estar presentes no lobby dentro do
+                    horário estipulado para o jogo (30 minutos de tolerância de
+                    atraso). Se os jogadores não estiverem presentes depois do
+                    limite, a equipa com todos os membros pode tirar uma captura
+                    de ecrã ao Lobby e reportar o resultado (Vitória por falta
+                    de comparência). 1.3 - Jogos serão transmitido ou gravados
+                    por algum observador da equipa VLRPT 2 - Equipas e Roster
+                    2.1 - Cada equipa deve conter no mínimo 5 jogadores. 2.2 -
+                    Cada roster pode conter 5 jogadores e 2 substitutos e 1
+                    treinador. 2.3 - Um jogador substituto pode ser adicionado
+                    antes ou depois de um jogo/mapa completo. 2.4 - O substituto
+                    não pode pertencer a mais nenhuma equipa que esteja a
+                    participar ou tenha sido eliminada no mesmo Torneio. 2.5 -
+                    Qualquer jogador do roster que não esteja a participar no
+                    jogo, pode preencher a vaga do treinador da equipa no lobby.
+                    2.6 - Cada jogador só pode representar uma equipa no mesmo
+                    Torneio. 3 - Exploits e Cheats 3.1 - Qualquer modificação de
+                    terceiros que crie uma injusta vantagem vai resultar na
+                    remoção imediata da equipa. Isto não sendo permitido:
+                    Multihacks Wallhack Aimbot Modelos Coloridos Sem Recoil Sem
+                    Flash Mudanças de Sons 3.2 - Todos os cheats estão
+                    estritamente proibidos em qualquer circunstancia do Torneio.
+                    Se um utilizador for apanhado a usar qualquer tipo de cheats
+                    irá resultar em Ban e desqualificação da equipa. Os
+                    seguintes cheats incluídos são: ESP Aim hacks Speed hacks
+                    Wallhacks Radar hacks Hitbox manipulação Teletransporte
+                    (qualquer tipo de transporte que não seja disponível no
+                    jogo) Uso de qualquer bug/bugs para ganhar vantagem contra
+                    os seus oponentes. 3.3 - Caso seja constatado que alguém
+                    está abusando qualquer uma das regras, as seguintes
+                    penalidades podem ser aplicadas: Aviso Vitória para o
+                    adversário Desqualificação do torneio 3.4 - Ferramentas de
+                    sobreposição de terceiros não são permitidas. Exemplos:
+                    Tracker Blitz Custom Crosshair 4 - Pausas e Desconexões 4.1
+                    - As pausas técnicas são apenas para dificuldades técnicas
+                    ou desconexões durante o início da ronda (fase de compra).
+                    4.2 - Na eventualidade de um jogador se desconectar a meio
+                    de uma ronda a ronda deverá continuar como normal. Quando a
+                    ronda acabar e a nova ronda começar o host pausa o lobby na
+                    fase de compra permitindo ao jogador/es se reconectem,
+                    voltando ao lobby, a equipa com problemas de conexão devem
+                    informar à outra equipa que estão prontos para continuar.
+                    4.3 - As pausas técnicas tem uma duração de 10 minutos
+                    (máximos). Caso seja necessário um tempo extra cabe ao
+                    observador da partida decidir se é justificável ou não.
+                    Quando ambas as equipas estiverem prontas para continuar
+                    devem usar o chat in-game para informar o observador (nunca
+                    o party voice chat). Usar a pausa técnica como pausa tática
+                    é proíbido. 4.4 - As equipas podem o Timeout in-game de 2x
+                    um minuto de pausa. Só podem usar uma por cada lado. Isto é
+                    permitido para todas as fases do Torneio. (Não devem usar a
+                    pausa do timer ingame para pausas táticas) 4.5 - No caso de
+                    Prorrogação, cada equipa irá ter o direito de mais uma pausa
+                    tática para usar na prorrogação. 5 - Streaming 5.1 – Os
+                    jogadores podem fazer live stream dos seus POVs durante os
+                    qualificadores abertos, desde que cumprem os requisitos.
+                    Jogadores a fazer Streaming tem de ter a live stream muda
+                    durante conversas no Discord com a Staff, incluindo
+                    encontros com os líderes antes do torneio começar.
+                    Treinadores, equipa staff e criadores de conteúdo não contam
+                    como jogadores competidores, e por tal não podem fazer
+                    stream. 5.2 - É permitido realizar Watch party’s do torneio
+                    através de uma stream de um observador. 5.3 - Todos os jogos
+                    das finais ao vivo do Torneio serão transmitidos nos canais
+                    próprios do Torneio. Apenas parceiros de produção aprovados
+                    e jogadores concorrentes poderão transmitir o evento
+                    principal. 5.4 - Os jogadores que transmitirem o evento
+                    serão obrigados a usar um atraso mínimo de 240 segundos para
+                    manter a integridade competitiva. Qualquer pessoa que
+                    transmita o torneio deve desativar a configuração "mostrar
+                    sangue" e "mostrar corpos", de acordo com as diretrizes da
+                    Riot. 5.5 - Os jogadores que transmitirem o evento também
+                    devem incluir o logotipo do Torneio em sua sobreposição e
+                    utilizar o título do evento no título da transmissão.
+                    Infelizmente, as equipes incapazes de atender a essa
+                    solicitação não poderão transmitir. 5.6 - Para todos os
+                    indivíduos que desejam transmitir, solicitamos educadamente
+                    o relatório de todas as estatísticas após o evento. 6 -
+                    FORMATO 6.1 - Divisão de equipas entre 2 ligas (
+                    「🏆」primeira-liga / 「🏆」segunda-liga ) de 8 equipas 6.2
+                    - Confrontos no formato Round Robin (todos jogam contra
+                    todos) com 1 volta 6.3 - Dependendo da posição na tabela da
+                    equipa no final da volta serão atribuídos pontos do circuito
+                    VLRPT que, posteriormente, darão a possibilidade de
+                    qualificar para um evento futuro 6.4 - A despromoção e
+                    promoção de equipas da 1° para a 2° liga, e vice-versa, será
+                    feita através de uma final four entre o 7° e 8° lugar da 1°
+                    liga e o 1° e 2° da 2° liga (double elimination bracket) 6.5
+                    - A qualificação para as ligas poderá ser feita através de
+                    um qualificador realizado ao mesmo tempo que as ligas
+                    (single elimination bracket) no qual apenas as equipas
+                    campeã e vice-campeã poderão aceder a uma final four contra
+                    o 7° e 8° da 2° liga 6.6 - A falta de comparência de 2 ou
+                    mais equipas das ligas custará no uso dos resultados atuais
+                    ( caso não se tenha terminado a primeira volta) ou os
+                    resultados da primeira volta (caso já tenha sido concluída)
+                    7 - Map poll and veto 7.1 - Map poll atual: Ascent Fracture
+                    Haven Icebox Lótus Pearl Split 7.2 - Processo do Veto: (o
+                    processo de veto poderá alterar face a um update de mapas no
+                    Valorant ou restrição da Riot) Em jogos à melhor de um
+                    (BO1): Ban equipa A Ban equipa B Ban equipa A Ban equipa B
+                    Ban equipa A Ban equipa B Side pick equipa A Em jogos à
+                    melhor de três (Bo3): Ban equipa A Ban equipa B Map pick
+                    equipa A Side pick equipa B Map pick equipa B Side pick
+                    equipa A Ban equipa A Ban equipa B Side pick equipa A 7.3 -
+                    Novos agentes terão um período de experiência de quatro (4)
+                    semanas. Se o torneio começar durante este período de
+                    experiência, o agente será proibido pelo resto do torneio. 8
+                    - Conduta do Jogador 8.1 - Espera-se que os jogadores tratem
+                    outros Competidores, Staff e Equipa de produção com respeito
+                    em qualquer altura. 8.2 - Nomes de equipes, nomes de
+                    jogadores, crosshairs e logotipos não devem ser ofensivos,
+                    tóxicos, inapropriado ou prejudiciais. Para a aplicação de
+                    todas as regras ou raras circunstâncias que possam estar
+                    fora das regras, a decisão da Staff é final.
+                </p>
+            </div>
+        </main>
+    );
 }
--- src/components/main_navbar.tsx
+++ src/components/main_navbar.tsx
@@ -1,31 +1,53 @@
+"use client";
 import Image from "next/image";
 import styles from "./main_navbar.module.css";
 
 export default function MainNavbar() {
+    const activePage = window.location.pathname.split("/")[1] || "home";
+    
+    const element = document.getElementById(activePage);
+    element?.classList.add(styles.active);
     return (
         <nav className={styles.nav}>
             <ul className={styles.container}>
                 <li className={styles.element}>
-                    <a href="/">Home</a>
+                    <a href="/" id="home">
+                        Início
+                    </a>
                 </li>
                 <li className={styles.element}>
-                    <a href="/info">Info</a>
+                    <a href="/info" id="info">
+                        Informações
+                    </a>
                 </li>
                 <li className={styles.element}>
-                    <a href="/regulamento">Regulamento</a>
+                    <a href="/regulamento" id="regulamento">
+                        Regulamento
+                    </a>
                 </li>
                 <ul className={styles.center}>
-                    <Image src="/logo.png" alt="logo" width={150} height={150} />
+                    <Image
+                        src="/logo.png"
+                        alt="logo"
+                        width={150}
+                        height={150}
+                    />
                 </ul>
                 <ul className={styles.right}>
                     <li className={styles.element}>
-                        <a href="/staff">Administração</a>
+                        <a href="/staff" id="staff">
+                            Administração
+                        </a>
                     </li>
                     <li className={styles.element}>
-                        <a href="/login">Login</a>
+                        <a href="/login" id="login">
+                            Entrar
+                        </a>
                     </li>
                     <li className={styles.element}>
-                        <a href="/register">Register</a>
+                        <a href="/register" id="register">
+                            Registar
+                        </a>
                     </li>
                 </ul>
             </ul>
