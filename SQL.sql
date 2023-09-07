4d43fa0ad12083c4d054aa4722dd85df803dfdbe
António Matos <antonio.martim.matos@gmail.com>
2023-09-08 00:23:06 +0100

    Update SQL

--- SQL.sql
+++ SQL.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Password` varchar(64) NOT NULL,
   `Email` varchar(320) NOT NULL,
   `Role` varchar(10) NOT NULL DEFAULT 'user',
-  `DiscordId` varchar(20) NOT NULL DEFAULT '0',
+  `DiscordData` varchar(300) NOT NULL DEFAULT '{}',
   `RegisterDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `LastLoginDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
@@ -36,8 +36,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 -- Dumping data for table vlrpt.users: ~1 rows (approximately)
 DELETE FROM `users`;
-INSERT INTO `users` (`ID`, `Username`, `Password`, `Email`, `Role`, `DiscordId`, `RegisterDate`, `LastLoginDate`) VALUES
-	(1, 'OOOOOOOOOOOOOOOO', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '123', 'user', '123', '2023-09-03 03:35:53', '2023-09-03 03:35:53');
+INSERT INTO `users` (`ID`, `Username`, `Password`, `Email`, `Role`, `DiscordData`, `RegisterDate`, `LastLoginDate`) VALUES
+	(1, 'admin', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '123', 'user', '{"id":"892504906576048138","username":"srd4rkoficial","avatar":"a_792edbe072322e68d90f48e5f761cded"}', '2023-09-03 03:35:53', '2023-09-03 03:35:53');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
